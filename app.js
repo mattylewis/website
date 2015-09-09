e62777a80b68e2bb2dfa43def2ad9f233513bb06
matty lewis <matthew@mglewis.co.uk>
2015-09-09 06:47:44 -0400

    Added setting app.locals.pretty = true; to make html look nice on client side

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
+app.locals.pretty = true;
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
