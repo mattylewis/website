5802b9924f5e64dfc66d588c802fccb06f19fc82
matty lewis <matthew@mglewis.co.uk>
2015-09-15 16:59:20 -0400

    improved jade layout and minor css cleanup

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Matthew Lewis' });
 });
 
-router.get('/projects', function(req, res, next) {
-	res.render('projects', { title: 'Personal Projects'})
-})
-
-router.get('/reading_list', function(req, res, next) {
-	res.render('reading_list', { title: 'Reading List'})
-})
-
 router.get('/error', function(req, res, next) {
 	res.render('error', { title: 'Error'})
 })
